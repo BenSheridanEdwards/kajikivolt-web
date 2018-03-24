9a35a18949b93bb164ce1bffda42100f2b3028fe
Dermot Byrne <dermotbyrne@dermot.local>
2018-03-24 10:44:26 +0000

    API routing

--- app.js
+++ app.js
@@ -18,12 +18,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('*/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
+
+// Serve static assets
+app.use(express.static(path.resolve(__dirname, 'client/build')));
+
 app.use('/api', apiRouter );
+
+// Always return the main index.html, so react-router render the route in the client
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+});
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
