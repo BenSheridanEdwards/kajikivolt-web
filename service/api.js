835d3bb3cba002dcd21110ce7138d3c0ef4381ea
Dermot Byrne <dermotbyrne@dermot.local>
2018-03-24 23:07:53 +0000

    Mailchimp subscibe component

--- service/api.js
+++ service/api.js
@@ -1,6 +1,11 @@
-var config = require('./../config/env.json')[process.env.NODE_ENV || 'development'];
+var config;
+if (process.env.NODE_ENV !== 'production') {
+  config = require('./../config/env.json')[process.env.NODE_ENV || 'development'].MAILCHIMP_APIKEY;
+} else {
+  config = process.env.MAILCHIMP_APIKEY
+}
 var MCapi = require('mailchimp-api');
-var MC = new MCapi.Mailchimp(config.MAILCHIMP_APIKEY);
+var MC = new MCapi.Mailchimp(config);
 
 let subscribe = function(mcReq, callback) {
   MC.lists.subscribe(mcReq, function(data) {
